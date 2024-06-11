f792381270a4fab187622fdbf6150a82e5236141
Deuteronome <o.burcker@e2c-grandlille.fr>
2024-06-11 16:51:05 +0200

    Menu déroulant

--- script/app.js
+++ script/app.js
@@ -90,6 +90,7 @@ else
 */
 //
 
+/*
 alert("Salut utilisateur");
 
 let userName = prompt("Quel est ton nom");
@@ -97,8 +98,13 @@ let userName = prompt("Quel est ton nom");
 alert (`Bonjour ${userName}`);
 
 let userAge = prompt('Quel est votre age?');
- 
-if(userAge >= 18)
+userAge = parseInt(userAge);
+
+
+if(isNaN(userAge)) {
+    alert('on t\'a demandé ton age pas ta vie');
+}
+else if(userAge >= 18)
 {
     alert("Tu es majeur");
 }
@@ -106,3 +112,68 @@ else
 {
     alert("tu es mineur");
 }
+*/
+/*
+let variableTest = 18;
+
+console.log(typeof variableTest);
+
+variableTest = "18";
+
+console.log(typeof variableTest);
+
+variableTest = prompt('notez 18');
+
+*/
+
+//Boucles
+/*
+let words = [];
+let word = "start"
+while(word != "stop")
+{
+    word = prompt('ecrivez stop pour vous stopper');
+    words.push(word);
+}
+
+for(let i =0; i<words.length; i++) {
+    console.log(words[i]);
+}
+
+
+/*
+let tableauTest = [23, 18, 'Oli', -35.2];
+tableauTest[8] = 'une nouvelle valeur';
+tableauTest[1]+=10;
+console.log(tableauTest);
+*/
+/*
+do {
+    // code à faire dans la boucle
+} while (/* condition )
+
+
+for(let i = 0; i<10; i++) {
+    console.log(i);
+}
+*/
+
+
+/**
+ * 
+ * @param {*} nombre1 
+ * @param {*} nombre2 
+ * @param {*} option 
+ * @returns 
+ */
+function addition(nombre1, nombre2, option)
+{
+   console.log('bulp');
+
+   return nombre1+nombre2;
+}
+
+addition(7, 2);
+let resultat = addition(13, 4);
+console.log(resultat);
+
